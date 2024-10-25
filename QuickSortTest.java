df63f786e2ec7a423ccd2e19eaf240bece9dd18b
rolandngoy <rolandngoy@yahoo.com>
2024-10-25 09:05:35 -0500

    Update QuickSortTest.java

--- QuickSortTest.java
+++ QuickSortTest.java
@@ -1,73 +1,36 @@
-public class QuickSortTest{
-  public static void main(String[] args){
-    // test partition()
-    int[] array = new int[]{5, 4, 3, 2, 1};
-    System.out.println("expect:\t2");
-    System.out.println("got:\t"+QuickSort.partition(array, 0, 4));
-    printArray(array);
-
-    array = new int[]{5, 4, 1, 2, 3};
-    System.out.println("expect:\t0");
-    System.out.println("got:\t"+QuickSort.partition(array, 0, 4));
-    printArray(array);
-
-    array = new int[]{3, 4, 5, 2, 1};
-    System.out.println("expect:\t4");
-    System.out.println("got:\t"+QuickSort.partition(array, 0, 4));
-    printArray(array);
-
-    // test sort()
-    array = new int[]{1};
-    System.out.println("Is the array sorted?");
-    System.out.println("expect:\ttrue");
-    QuickSort.sort(array);
-    System.out.println("got:\t"+isAscending(array));
-    printArray(array);
-
-    array = new int[]{2, 1};
-    System.out.println("Is the array sorted?");
-    System.out.println("expect:\ttrue");
-    QuickSort.sort(array);
-    System.out.println("got:\t"+isAscending(array));
-    printArray(array);
-
-    array = new int[]{3, 2, 1};
-    System.out.println("Is the array sorted?");
-    System.out.println("expect:\ttrue");
-    QuickSort.sort(array);
-    System.out.println("got:\t"+isAscending(array));
-    printArray(array);
-
-    array = new int[]{5, 4, 3, 2, 1};
-    System.out.println("Is the array sorted?");
-    System.out.println("expect:\ttrue");
-    QuickSort.sort(array);
-    System.out.println("got:\t"+isAscending(array));
-    printArray(array);
-
-    array = new int[100];
-    for(int i=0; i<100; i++){
+public class QuickSortTest
+{
+  public static void main(String[] args)
+  {
+    int[] array = new int[100];
+    for(int i=0; i<100; i++)
+    {
       array[i] = 100-i;
     }
     System.out.println("Is the array sorted?");
-    System.out.println("expect:\ttrue");
+    System.out.println("Expect: \ttrue");
     QuickSort.sort(array);
-    System.out.println("got:\t"+isAscending(array));
-    printArray(array);
+    //System.out.println("got:\t"+isAscending(array));
+    //printArray(Array);
   }
 
-  public static void printArray(int[] array){
-    for(int i=0; i<array.length; i++){
-      System.out.print(array[i]+" ");
+  public static void printArray(int[] array)
+  {
+    for(int i=0; i<array.length; i++)
+    {
+      System.out.print(array[i]+ " ");
     }
     System.out.println();
   }
 
-  public static boolean isAscending(int[] array){
+  public static boolean isAscending(int[] array)
+  {
     boolean result = true;
-    for(int i=0; i<array.length-1; i++){
-      if(array[i] > array[i+1]){
-        // not sorted
+    for(int i=0; i<array.length-1; i++)
+    {
+      if(array[i] > array[i+1])
+      {
+        //not sorted
         result = false;
         break;
       }
